a435e0f1ec02675b6ac5724374c6d8d06b44d03a
yuyu78 <ishiiyuki78@gmail.com>
2021-09-03 21:52:08 +0000

    Add page 500 error

--- app.py
+++ app.py
@@ -197,6 +197,11 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 
+@app.errorhandler(500)
+def internal_error(e):
+    return render_template('500.html'), 500
+
+    
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
