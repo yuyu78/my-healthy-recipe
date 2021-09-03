074e32229f49c642ccfe8fce1e1b9769ec52e864
yuyu78 <ishiiyuki78@gmail.com>
2021-09-03 13:25:29 +0000

    Add custom materialize select validation

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,14 @@ $(document).ready(function(){
     $(".sidenav").sidenav({edge: "right"});
     $('.modal').modal();
     $('select').formSelect();
+
+    // Credit: Code Institute Mini Project > Adding a task> Materialize Form Validation https://www.youtube.com/watch?v=CG36uQtAzkU&t=279s&ab_channel=MediaUpload
+    validateMaterializeSelect();
+    function validateMaterializeSelect() {
+        if ($("select.validate").prop("required")) {
+            $("select.validate").css({ "display": "block", "height": "0", "padding": "0", "width": "0", "position": "absolute" });
+        }
+    }
   });
 
 (function () {
