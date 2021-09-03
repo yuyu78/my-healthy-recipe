940026c4e8e04d460f82bce713c9616425602df6
yuyu78 <ishiiyuki78@gmail.com>
2021-09-03 21:37:02 +0000

    Add page 404 error

--- app.py
+++ app.py
@@ -191,6 +191,12 @@ def delete_recipe(recipe_id):
     return redirect(url_for("get_recipes"))
 
 
+# -------------------------------------- error page
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
